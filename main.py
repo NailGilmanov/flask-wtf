1e576754e3d6a285ccf8538b039bce16cea4b4c1
NailGilmanov <NailGIlmanov27@mail.ru>
2022-03-10 20:42:42 +0300

    Список профессий

--- main.py
+++ main.py
@@ -23,6 +23,31 @@ def training(prof):
     return render_template('training.html', engineer_profs=engineer_profs)
 
 
+@app.route('/list_prof/<string:type>')
+def list_prof(type):
+    profs = [
+        "инженер-исследователь",
+        "пилот",
+        "строитель ",
+        "экзобиолог",
+        "врач",
+        "инженер по терраформированию",
+        "климатолог",
+        "специалист по радиационной защите",
+        "астрогеолог",
+        "гляциолог",
+        "инженер жизнеобеспечения",
+        "метеоролог",
+        "оператор марсохода",
+        "киберинженер",
+        "штурман",
+        "пилот дронов"
+    ]
+    return render_template('list_prof.html',
+                           type=type,
+                           profs=profs)
+
+
 def main():
     app.run()
 
